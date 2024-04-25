1ac0a85a973f4ed9a23b5e919146c9f1fd6370f0
Spherox <65167045+NathanielJames97@users.noreply.github.com>
2024-04-25 12:40:14 +0100

    Add multiple ip's to test, and built to handle.

--- Exercise - Build code with methods/Program.cs
+++ Exercise - Build code with methods/Program.cs
@@ -9,19 +9,26 @@ then ipAddress is valid
 
 else ipAddress is invalid
 */
-string ipv4Input = "107.31.1.5";
+string[] ipv4Input = {"107.31.1.5", "255.0.0.255", "555..0.555", "255...255"};
 bool validLength = false;
 bool validZeroes = false;
 bool validRange = false;
 
 
-if (validLength && validZeroes && validRange)
+foreach (string ip in ipv4Input) 
 {
-    Console.WriteLine($"ip is a valid IPv4 address");
-} 
-else 
-{
-    Console.WriteLine($"ip is an invalid IPv4 address");
+    ValidateLength(); 
+    ValidateZeroes(); 
+    ValidateRange();
+
+    if (validLength && validZeroes && validRange) 
+    {
+        Console.WriteLine($"{ip} is a valid IPv4 address");
+    } 
+    else 
+    {
+        Console.WriteLine($"{ip} is an invalid IPv4 address");
+    }
 }
 void ValidateLength() {
     string[] address = ipv4Input.Split(".");
