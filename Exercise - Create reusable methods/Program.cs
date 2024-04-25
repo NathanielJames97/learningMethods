3330811ca19505dee9cf4d62a09d61de1e529d42
Spherox <65167045+NathanielJames97@users.noreply.github.com>
2024-04-25 12:01:28 +0100

    Replace other instance of unuse DisplayTimes code

--- Exercise - Create reusable methods/Program.cs
+++ Exercise - Create reusable methods/Program.cs
@@ -39,30 +39,7 @@ else
 }
 
 Console.WriteLine("New Medicine Schedule:");
-
-/* Format and display medicine times */
-foreach (int val in times)
-{
-    string time = val.ToString();
-    int len = time.Length;
-
-    if (len >= 3)
-    {
-        time = time.Insert(len - 2, ":");
-    }
-    else if (len == 2)
-    {
-        time = time.Insert(0, "0:");
-    }
-    else
-    {
-        time = time.Insert(0, "0:0");
-    }
-
-    Console.Write($"{time} ");
-}
-
-Console.WriteLine();
+DisplayTimes();
 
 void DisplayTimes()
 {
